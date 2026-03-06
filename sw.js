0cbd46e6b37a9de3180a27770d1822fd572e271f
PhysiqueRUSH <physiquerush@gmail.com>
2026-03-06 11:04:03 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -57,7 +57,7 @@ self.addEventListener('notificationclick', function(e) {
   }));
 });
 
-const CACHE = 'physioflow-pro-v63';
+const CACHE = 'physioflow-pro-v64';
 const ASSETS = ['./index.html', './manifest.json'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
